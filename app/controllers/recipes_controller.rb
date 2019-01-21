174ae13353a587396d8a6d4fc6447238b3ed1b2b
Judy McGoldrick <jjeanius@verizon.net>
2019-01-21 03:26:49 +0000

    Automatically backed up by Learn

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -22,7 +22,7 @@ class RecipesController < ApplicationController
 private
 
   def receipe_params
-    params.require(:recipe).permit(:title, :ingredient => [:attributes_0_name, :attributes_0_quantity, :attributes_1_name, :attributes_1_quantity])
+    params.require(:recipe).permit(:title, :name, :quantity)
   end
 
 
