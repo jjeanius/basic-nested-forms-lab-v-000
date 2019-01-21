acdd76fc24261c4a3cff2c033a5c5f85997a31f0
Judy McGoldrick <jjeanius@verizon.net>
2019-01-21 02:13:05 +0000

    Automatically backed up by Learn

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,4 +1,5 @@
 class RecipesController < ApplicationController
+
   def show
     @recipe = Recipe.find(params[:id])
   end
@@ -9,8 +10,20 @@ class RecipesController < ApplicationController
 
   def new
     @recipe = Recipe.new
+    @recipe.ingredients.build(:quantity=> "quantity")
+    @recipe.ingredients.build(:name=> "name")
+
   end
 
   def create
+    @recipe = Recipe.create(receipe_params)
+    redirect_to recipe_path
   end
+
+  def receipe_params
+    params.require(:recipe).permit(:title, :ingredient => [:name_0, :quanity_0, :name_1, :quantity_1])
+  end
+
+
+
 end
